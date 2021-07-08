23623081277927a06d57fc30d6155b2fa44b60b7
Matteo Granziera <matteo.granziera@molo17.com>
2021-07-08 11:36:42 +0000

    feat: update waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from os import close
 import rospy
 from geometry_msgs.msg import PoseStamped
 from rospy.timer import sleep
+from std_msgs.msg import Int32
 from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
 import numpy as np
@@ -25,6 +27,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+MAX_DECEL = 1.0
 
 
 class WaypointUpdater(object):
@@ -34,17 +37,17 @@ class WaypointUpdater(object):
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-        # rospy.Subscriber('/traffic_waypoint', Lane, self.traffic_cb)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        # TODO: Add a subscriber for /obstacle_waypoint below
         # rospy.Subscriber('/obstacle_waypoint', Lane, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
         self.pose = None
         self.base_waypoints = None
         self.waypoints_2d = None
         self.waypoints_tree = None
+        self.stopline_waypoint_index = -1
 
         # rospy.spin()
         self.loop()
@@ -54,9 +57,8 @@ class WaypointUpdater(object):
         rate = rospy.Rate(50)
         rospy.loginfo("Loop started!")
         while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints:
+            if self.pose and self.base_waypoints and self.waypoints_tree:
                 closest_waypoint_index = self.get_closest_waypoint_index()
-                rospy.loginfo("Closest waiypoint index: %s", closest_waypoint_index)
                 self.publish_waypoints(closest_waypoint_index)
             rate.sleep()
     
@@ -81,15 +83,39 @@ class WaypointUpdater(object):
     def publish_waypoints(self, closest_index):
         lane = Lane()
         lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoints[closest_index:closest_index + LOOKAHEAD_WPS]
+        farthest_index = closest_index + LOOKAHEAD_WPS
+        waypoints = self.base_waypoints.waypoints[closest_index:farthest_index]
+
+        if self.stopline_waypoint_index == -1 or self.stopline_waypoint_index >= farthest_index:
+            lane.waypoints = waypoints
+        else:
+            lane.waypoints = self.decelerate_waypoints(waypoints, closest_index)
         
         self.final_waypoints_pub.publish(lane)
+
+    def decelerate_waypoints(self, waypoints, closest_index):
+        final_waypoints = []
+
+        for i, wp in enumerate(waypoints):
+            waypoint = Waypoint()
+            waypoint.pose = waypoint.pose
+
+            stop_index = max(self.stopline_waypoint_index - closest_index - 3, 0)
+            dist = self.distance(waypoints, i, stop_index)
+            velocity = math.sqrt(2*MAX_DECEL*dist)
+
+            if velocity < 1.0:
+                velocity = 0
+
+            waypoint.twist.twist.linear.x = min(velocity, wp.twist.twist.linear.x)
+            final_waypoints.append(waypoint)
+
+        return final_waypoints
 
     def pose_cb(self, msg):
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        rospy.loginfo("Waypoints received!")
 
         # This is called one time
         self.base_waypoints = waypoints
@@ -98,8 +124,7 @@ class WaypointUpdater(object):
             self.waypoints_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        self.stopline_waypoint_index = msg.data
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
